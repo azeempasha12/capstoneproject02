68df63172bb933127f7c9da31fb8ad54d9926737
azeempasha12 <tm784760@gmail.com>
2023-10-12 18:03:07 +0530

    hello

---
+++ pasha2.js
@@ -0,0 +1,11 @@
+function calculateTip(billAmount, tipPercentage) {
+    const tipAmount = billAmount * tipPercentage;
+    const totalAmount = billAmount + tipAmount;
+    return Number(totalAmount.toFixed(2));
+}
+
+// Example1
+const billAmount = 60.75; // Replace with your bill amount
+const tipPercentage = 0.25; // 25% tip as 0.25
+const total = calculateTip(billAmount, tipPercentage);
+console.log(`Total amount to be paid: $${total}`); //$75.94
