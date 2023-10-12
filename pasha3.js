300cb2765979c922f6ec93c1c84f8a1f75cd948a
azeempasha12 <tm784760@gmail.com>
2023-10-12 18:26:02 +0530

    hi3

---
+++ pasha3.js
@@ -0,0 +1,16 @@
+function isPalindrome(str) {
+    // Step 1: Convert the input string to lowercase
+    const lowerCaseStr = str.toLowerCase();
+    
+    // Step 2: Remove non-alphanumeric characters (including spaces and punctuation)
+    const cleanStr = lowerCaseStr.replace(/[^a-z0-9]/g, "");
+
+    // Step 3: Compare the modified string with its reverse
+    const reversedStr = cleanStr.split("").reverse().join("");
+
+    return cleanStr === reversedStr;
+}
+
+// Example usage:
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello")); // false
